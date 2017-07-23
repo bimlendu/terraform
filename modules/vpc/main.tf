643242319c06c4063f61957f5d3b57edbfc420d7
Bimlendu Mishra <justbimlendu@gmail.com>
2017-07-23 11:19:57 +0530

    format tags

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,18 +12,14 @@ resource "aws_vpc" "default" {
   enable_classiclink               = "${lookup(var.vpc, "enable_classiclink")}"
   assign_generated_ipv6_cidr_block = "${lookup(var.vpc, "assign_generated_ipv6_cidr_block")}"
 
-  tags = "${merge(var.default_tags, map(
-    "Name", format("%s-vpc", var.name)
-  ))}"
+  tags = "${merge(var.default_tags, map( "Name", "${var.name}-vpc" ))}"
 }
 
 # add an internet gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = "${aws_vpc.default.id}"
 
-  tags = "${merge(var.default_tags, map(
-    "Name", format("%s-igw", var.name)
-  ))}"
+  tags = "${merge(var.default_tags, map( "Name", "${var.name}-igw" ))}"
 }
 
 # add public subnets in each available zones, limited by ${var.num_zones}
@@ -37,9 +33,7 @@ resource "aws_subnet" "public" {
 
   map_public_ip_on_launch = true
 
-  tags = "${merge(var.default_tags, map(
-    "Name", format("%s-public-%s", var.name, element(data.aws_availability_zones.available.names, count.index))
-  ))}"
+  tags = "${merge(var.default_tags, map( "Name", "${var.name}-public-${element(data.aws_availability_zones.available.names, count.index)}"))}"
 }
 
 # add private subnets in each available zones, limited by ${var.num_zones}
@@ -51,18 +45,14 @@ resource "aws_subnet" "private" {
   cidr_block        = "${cidrsubnet(lookup(var.vpc, "cidr_block"), ceil(log(2 * length(slice(data.aws_availability_zones.available.names,0,min(lookup(var.vpc, "spread_across"), length(data.aws_availability_zones.available.names)))), 2)), count.index + length(slice(data.aws_availability_zones.available.names,0,min(lookup(var.vpc, "spread_across"), length(data.aws_availability_zones.available.names)))))}"
   availability_zone = "${element(data.aws_availability_zones.available.names, count.index)}"
 
-  tags = "${merge(var.default_tags, map(
-    "Name", format("%s-private-%s", var.name, element(data.aws_availability_zones.available.names, count.index))
-  ))}"
+  tags = "${merge(var.default_tags, map( "Name", "${var.name}-private-${element(data.aws_availability_zones.available.names, count.index)}"))}"
 }
 
 # add public route table and its route association
 resource "aws_route_table" "public" {
   vpc_id = "${aws_vpc.default.id}"
 
-  tags = "${merge(var.default_tags, map(
-    "Name", format("%s-rt-public", var.name)
-  ))}"
+  tags = "${merge(var.default_tags, map( "Name", "${var.name}-rt-public"))}"
 }
 
 resource "aws_route" "public_internet_gateway" {
@@ -91,9 +81,7 @@ resource "aws_route_table" "private" {
   vpc_id = "${aws_vpc.default.id}"
   count  = "${length(slice(data.aws_availability_zones.available.names,0,min(lookup(var.vpc, "spread_across"), length(data.aws_availability_zones.available.names))))}"
 
-  tags = "${merge(var.default_tags, map(
-    "Name", format("%s-rt-private-%s", var.name, count.index)
-  ))}"
+  tags = "${merge(var.default_tags, map( "Name", "${var.name}-rt-private-${count.index}"))}"
 }
 
 # add a nat gateway to each private subnet's route table
