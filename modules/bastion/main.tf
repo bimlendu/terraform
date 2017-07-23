62d6360e4b651be97aa0186f806197c78e1d4bf2
Bimlendu Mishra <justbimlendu@gmail.com>
2017-07-23 11:19:06 +0530

    move ami and ssh_key_pair out of module

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -1,19 +1,3 @@
-data "aws_ami" "bastion" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_security_group" "bastion" {
   name        = "bastion"
   description = "Allow ssh access to bastion from var.allowed_networks."
@@ -36,15 +20,10 @@ resource "aws_security_group" "bastion" {
   tags = "${merge(var.default_tags, map( "Name", "${var.name}-bastion-sg"))}"
 }
 
-resource "aws_key_pair" "bastion" {
-  key_name_prefix = "bastion-"
-  public_key      = "${lookup(var.bastion, "ssh_public_key")}"
-}
-
 resource "aws_instance" "bastion" {
-  ami                    = "${data.aws_ami.bastion.id}"
+  ami                    = "${lookup(var.bastion, "ami")}"
   instance_type          = "${lookup(var.bastion, "instance_type")}"
-  key_name               = "${aws_key_pair.bastion.key_name}"
+  key_name               = "${lookup(var.bastion, "key_name")}"
   subnet_id              = "${lookup(var.bastion, "subnet_id")}"
   vpc_security_group_ids = ["${aws_security_group.bastion.id}"]
 
